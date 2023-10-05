bdd996aca336c99a3afe282af0117bbb33dfad65
MihajloW <84952137+MihajloW@users.noreply.github.com>
2023-10-05 21:41:47 +0200

    gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -7,6 +7,7 @@ apt -y install nano
 apt -y install htop
 apt -y install nvtop
 apt -y install cmake
+apt -y install python3-pip
 
 # Clone the repository and build the project
 git clone https://github.com/shanhaicoder/XENGPUMiner.git
@@ -15,9 +16,8 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x692C8afeaD6F4B37fCCE0F9d1233a3f956EC82f4/g' config.conf
+sed -i 's/account = 0x692C8afeaD6F4B37fCCE0F9d1233a3f956EC82f4/account = 0x692C8afeaD6F4B37fCCE0F9d1233a3f956EC82f4/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
-nohup python miner.py --gpu=true > miner.log 2>&1 &
-nohup ./xengpuminer > xengpuminer.log 2>&1 &
+nohup ./miner.sh > miner.log 2>&1 &
