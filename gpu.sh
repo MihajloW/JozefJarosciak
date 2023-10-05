845c550d9f20a985e0ab01bb3215ccccafce2b8f
MihajloW <84952137+MihajloW@users.noreply.github.com>
2023-10-05 22:00:13 +0200

    gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x692C8afeaD6F4B37fCCE0F9d1233a3f956EC82f4/account = 0x692C8afeaD6F4B37fCCE0F9d1233a3f956EC82f4/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x692C8afeaD6F4B37fCCE0F9d1233a3f956EC82f4/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
